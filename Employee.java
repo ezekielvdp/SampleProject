95248b2a5f4c49c2abf4663d289a27f99a53045d
Ezekiel dela Peña <ezekielvdp@yahoo.com>
2017-11-21 20:32:52 +0800

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,15 +1,25 @@
 public class Employee {
-	private int employeeId;
+	private int id;
+	private String firstName;
 
 	public Employee() {
-		this.employeeId = 1;
+		this.id = 1;
+		this.firstName = "Juan";
 	}
 	
-	public void setEmployeeId (int employeeId) {
-		this.employeeId = employeeId;
+	public void setId (int id) {
+		this.id = id;
 	}
 
-	public int getEmployeeId() {
-		return employeeId;
+	public int getId() {
+		return id;
+	}
+	
+	public void setfirstName (int firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
 	}
 }
